d023a33cca6bc58fe2a35f50f3b63ae8f7abfbf9
SophieCurinier <sophie.curinier@etu.emse.fr>
2021-11-12 12:18:21 +0100

    Home Screen creation button and buttons design modification

---
+++ app/src/main/java/com/example/cartel2022/MatchActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cartel2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MatchActivity : AppCompatActivity() {
+    private val itemsList = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match)
+        title = "Football"
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        customAdapter = CustomAdapter(itemsList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+        prepareItems()
+    }
+    private fun prepareItems() {
+        itemsList.add("Match 1")
+        itemsList.add("Match 2")
+        itemsList.add("Rugby M")
+        itemsList.add("Rugby F")
+        itemsList.add("Handball M")
+        itemsList.add("Handball F")
+        itemsList.add("Volley M")
+        itemsList.add("Volley F")
+        itemsList.add("Natation")
+        itemsList.add("Boxe")
+        itemsList.add("VTT")
+        itemsList.add("DJ Contest")
+        itemsList.add("Pompom")
+        customAdapter.notifyDataSetChanged()
+    }
+}
