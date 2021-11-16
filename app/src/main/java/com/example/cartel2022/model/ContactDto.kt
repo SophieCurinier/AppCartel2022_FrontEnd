9e4fbc38af0b504b80324adf39e9559bc75b41be
Pierre Maindron <pierre.maindron@etu.emse.fr>
2021-11-16 21:44:13 +0100

    Commentaires et dernières modifs

--- app/src/main/java/com/example/cartel2022/model/ContactDto.kt
+++ app/src/main/java/com/example/cartel2022/model/ContactDto.kt
@@ -1,3 +1,3 @@
 package com.example.cartel2022.model
-
-data class ContactDto(val id: Long, val poste: String, val circonstance: String?, val nom: String, val numero: String)
+//Définition de la data class permettant de définir les attributs de ContactDto: id, poste, circonstances d'appel, nom et numéro de téléphone.
+data class ContactDto(val id: Long, val poste: String, val circonstance: String, val nom: String, val numero: String)
--- app/src/main/java/com/example/cartel2022/model/ContactService.kt
+++ app/src/main/java/com/example/cartel2022/model/ContactService.kt
@@ -2,7 +2,7 @@ package com.example.cartel2022.model
 
 class ContactService {
     companion object {
-        // Contacts
+        // Définitions des données des différents contacts des responsables de l'association Cartel.
         val CONTACTS: List<ContactDto> = listOf(
             ContactDto(1, "Président", "Si les VP ne sont pas disponibles", "Ryan KAOUKEB", "07 67 40 42 36"),
             ContactDto(2, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
@@ -11,7 +11,7 @@ class ContactService {
             ContactDto(5, "Responsable Sports", "Litige important durant une compétition ou question sur un règlement sportif", "Julian Paquerot", "06 40 16 41 03"),
             ContactDto(6, "Responsable Hébergement", "Hébergements Hors Saint-Étienne", "Ajejandro Lopez", "07 67 24 70 81"),
             ContactDto(7, "Responsable Hébergement", "Hébergements dans Saint-Étienne", "Alan Iziquiel", "06 45 98 98 76"),
-            ContactDto(8, "Responsabe Restauration", "Problème de restauration","Cassiopée André", "06 87 77 40 44"),
+            ContactDto(8, "Responsabe Restauration", "Problème de restauration","Cassiopée André", "07 78 35 97 56"),
             ContactDto(9, "Responsable Transports", "Problème de transports en journée", "Matthieu Godillon", "06 87 77 40 44"),
             ContactDto(10, "Responsable Transports", "Problème de transport en soirée", "Raphaël Klaja", "06 29 53 42 51")
 
