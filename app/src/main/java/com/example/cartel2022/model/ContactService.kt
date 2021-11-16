bb592af8c88a89f081bd775b923067db58d98b62
Pierre Maindron <pierre.maindron@etu.emse.fr>
2021-11-16 20:01:20 +0100

    Mise à jour contacts

--- app/src/main/java/com/example/cartel2022/model/ContactService.kt
+++ app/src/main/java/com/example/cartel2022/model/ContactService.kt
@@ -8,17 +8,17 @@ class ContactService {
             ContactDto(2, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
             ContactDto(3, "Vice-Président", "Relation avec les délégations", "Philippe JESTIN", "06 87 77 40 45"),
             ContactDto(4, "Vice-Président", "Sports, Soirées", "Baptiste ABÉLÈS", "06 87 77 40 46"),
-            ContactDto(5, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
-            ContactDto(6, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
-            ContactDto(7, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
-            ContactDto(8, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
-            ContactDto(9, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44"),
-            ContactDto(10, "Vice-Président", "Hébergement, Transports, Restauration", "Pierre MAINDRON", "06 87 77 40 44")
+            ContactDto(5, "Responsable Sports", "Litige important durant une compétition ou question sur un règlement sportif", "Julian Paquerot", "06 40 16 41 03"),
+            ContactDto(6, "Responsable Hébergement", "Hébergements Hors Saint-Étienne", "Ajejandro Lopez", "07 67 24 70 81"),
+            ContactDto(7, "Responsable Hébergement", "Hébergements dans Saint-Étienne", "Alan Iziquiel", "06 45 98 98 76"),
+            ContactDto(8, "Responsabe Restauration", "Problème de restauration","Cassiopée André", "06 87 77 40 44"),
+            ContactDto(9, "Responsable Transports", "Problème de transports en journée", "Matthieu Godillon", "06 87 77 40 44"),
+            ContactDto(10, "Responsable Transports", "Problème de transport en soirée", "Raphaël Klaja", "06 29 53 42 51")
 
 
         )
 
-        fun findAll() = CONTACTS.sortedBy { it.poste }
+        fun findAll() = CONTACTS.sortedBy { it.id }
     }
 
 }
